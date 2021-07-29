de1ad64325daeba518585943ef2b5b43fcb55d00
CreatorsHut <sosgxv@naver.com>
2021-07-29 18:34:34 +0900

    antd 기본적용

--- src/product/index.js
+++ src/product/index.js
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { data } from "browserslist";
+import "./index.css";
 
 function ProductPage() {
   const { id } = useParams();
@@ -9,7 +10,7 @@ function ProductPage() {
   useEffect(function () {
     axios
       .get(
-        `https://341cf269-c712-4751-a587-2c7fd1b972ec.mock.pstmn.io/products/${id}`
+        `https://76647419-d04a-4ef6-a529-910fd75cb245.mock.pstmn.io/products/${id}`
       )
       .then(function (result) {
         setProduct(result.data);
@@ -18,8 +19,28 @@ function ProductPage() {
         console.error(error);
       });
   }, []);
-  console.log(product);
-  return <h1>상품 상세 페이지 {id} 상품 </h1>;
+
+  if (product === null) {
+    return <h1>상품 정보를 받고 있습니다...</h1>;
+  }
+
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.imageUrl} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seller}</span>
+      </div>
+      <div id="contents-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}</div>
+        <div id="createdAt">2020년 12월 8일</div>
+        <div id="description">{product.description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
